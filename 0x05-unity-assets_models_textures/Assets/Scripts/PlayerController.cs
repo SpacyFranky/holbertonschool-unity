25384144926493c2da87c9657a775e979abda1ae
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-09-22 16:41:29 +0100

    Adding movement and jumping controls

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w") && !Input.GetKey("s"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
+        }
+        else if (Input.GetKey("w") && !Input.GetKey(KeyCode.LeftShift) && !Input.GetKey("s"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey("s") && !Input.GetKey("w"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+        }
+
+        if (Input.GetKey("a") && !Input.GetKey("d"))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
+
+        if(Input.GetKey("space") && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce *Time.deltaTime, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
+}
