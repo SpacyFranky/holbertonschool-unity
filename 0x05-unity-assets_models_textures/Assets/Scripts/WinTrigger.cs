58a32d96300049267615b19ace1c82d5ea7fb1dc
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-09-23 04:39:56 +0100

    Stopping the timer at the finish line

--- 0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WinTrigger : MonoBehaviour
 {
+    public GameObject player;
+    public Text timerText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,12 @@ public class WinTrigger : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.GetComponent<Timer>().enabled = false;
+        timerText.color = new Color(0, 255, 0);
+        timerText.fontSize = 90;
     }
 }
