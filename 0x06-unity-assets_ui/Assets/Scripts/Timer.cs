d6d3ac056e5e39c33473e32a96bc9b3a187c88e5
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-09-23 15:54:18 +0100

    Building up a new project

---
+++ 0x06-unity-assets_ui/Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text timerText;
+    private float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        var minutes = time / 60;
+        var seconds = time % 60;
+        var fraction = (time * 100) % 100;
+
+        timerText.text = string.Format("{0:0}:{1:00}.{2:00}", minutes, seconds, fraction);
+    }
+}
---
+++ 0x06-unity-assets_ui/README.md
@@ -0,0 +1,8 @@
+# 0x06. Unity - Assets: UI
+# Assets Used
+* [Skyboxes: Farland Skies - Cloudy Crown](https://assetstore.unity.com/packages/2d/textures-materials/sky/farland-skies-cloudy-crown-60004)
+* [Models: Kenney's Nature Pack Extended](https://kenney.nl/assets/nature-pack-extended)
+# How to play
+- WASD to move and look around with the mouse<br>
+- Space bar to jump<br>
+- Shift button with W to boost<br>
