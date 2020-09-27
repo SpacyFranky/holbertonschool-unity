32244bc083c0d1ebc6ac5a8b938ded36fc269d7e
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2020-09-27 18:16:30 +0100

    Adding scenes to the build and button functions

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Loads the selected level
+    public void LevelSelect(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    // Quits game
+    public void QuitGame()
+    {
+        Debug.Log("Exited");
+        Application.Quit ();
+    }
+}
