b9541b058e5b14440f10f48dad177c595de9f1bd
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2021-01-27 13:27:44 +0100

    Adding background music to level01

---
+++ 0x08-unity-audio/Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    AudioSource bgMusic;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgMusic = GetComponent<AudioSource>();
+        bgMusic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bgMusic.Stop();
+    }
+}
