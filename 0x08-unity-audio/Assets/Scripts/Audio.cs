921069d3fec7b307689090da10f786522a59a306
SpacyFranky <52805559+SpacyFranky@users.noreply.github.com>
2021-01-27 11:32:24 +0100

    Adding sound to walking and fall impact animation

---
+++ 0x08-unity-audio/Assets/Scripts/Audio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    AudioSource running;
+    AudioSource falling;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource[] sources = GetComponents<AudioSource>();
+        running = sources[0];
+        falling = sources[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void PlayerFootstepSound()
+    {
+        running.Play();
+    }
+
+    private void PlayerFallingImpactSound()
+    {
+        falling.Play();
+    }
+}
